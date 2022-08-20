68d097b0e9fe73439bb4a7853684641d0fa14cad
François WAXIN <francois.waxin@icloud.com>
2022-08-20 21:38:21 +0200

    feat: Adding React Router v6 (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import ViteDemo from './pages/ViteDemo';
+import ViteDemoHome from './pages/ViteDemoHome';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Routes>
+      {/**
+       * The following route object is used as a layout route.
+       * Each child will inherit the layout described by the ViteDemo component
+       *
+       * Note : we'll have to call an <Outlet /> component for each parent in order to achieve that.
+       * (set in <ViteDemo /> if you want to see how it's implemented)
+       */}
+      <Route path="/" element={<ViteDemo />}>
+        <Route index element={<ViteDemoHome />} />
+      </Route>
+    </Routes>
   );
 }
 
